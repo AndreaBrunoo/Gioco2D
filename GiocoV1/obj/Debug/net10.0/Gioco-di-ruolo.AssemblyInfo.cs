42052cb7d26df5576bd93fee6827886f36a34abe
FabTam <fabiotamma90@gmail.com>
2026-06-15 12:08:05 +0200

    Implementazioni con chatGPT

--- GiocoV1/obj/Debug/net10.0/Gioco-di-ruolo.AssemblyInfo.cs
+++ GiocoV1/obj/Debug/net10.0/Gioco-di-ruolo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gioco-di-ruolo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdcc7f05f6d803e04881d62552bca7c0a8fc8716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe874dd57d72c3e9fa9568f54d24082e7e10448")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gioco-di-ruolo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gioco-di-ruolo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
