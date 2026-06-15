cdcc7f05f6d803e04881d62552bca7c0a8fc8716
AndreaBrunoo <andrea.bruno.06.ge@gmail.com>
2026-06-15 09:16:31 +0200

    documentazione

--- GiocoV1/obj/Debug/net10.0/Gioco-di-ruolo.AssemblyInfo.cs
+++ GiocoV1/obj/Debug/net10.0/Gioco-di-ruolo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gioco-di-ruolo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bda52c96b220025fe9b8b54c344a71595a202d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829ef5ee89967ba3a1789ef49c60535d579e4667")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gioco-di-ruolo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gioco-di-ruolo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
